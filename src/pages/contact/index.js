b9a22a6e0cacbce95d3af5b39651f6e583332c5c
Samuel <tamueka@hotmail.com>
2018-12-13 13:24:22 +0100

    Formulario contacto

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,8 +1,10 @@
+
 import { updateHeader } from 'components/header/header-component';
 import { updateContactForm } from 'components/contact-form/contact-form-component';
 
 import 'styles/main.scss';
 
+console.log('NO ME RECONOCE JAVASCRIPT P0R CONSOLA')
 
 updateHeader({ title: 'contact', active: 'contact' });
 updateContactForm();
